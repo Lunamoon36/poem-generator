6f97e837688964d14671589a752e23c22960d083
Lunamoon36 <150391744+Lunamoon36@users.noreply.github.com>
2024-02-27 14:08:12 +0000

    amended signature from AI

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let apiKey = "773b1fb672t5o4902c27a62f4db0b82a";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value},`;
   let context =
-    "You are a poem expert and love to write short poems. Please generate a four line poem in basic HTML. Please separate the sentences onto new lines. Please ensure to follow the user instructions. Please do not include a title. Please sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning ";
+    "You are a poem expert and love to write short poems. Please generate a four line poem in basic HTML. Please separate the sentences onto new lines. Please ensure to follow the user instructions. Please do not include a title. Please sign the poem with 'Cherry AI' inside a <strong> element at the end of the poem and NOT at the beginning ";
 
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
